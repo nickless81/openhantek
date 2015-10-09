ef40f70235ffba78eb79fd6b6a2e87a66f03ffef
Jose Pablo Castro Valverde <josep.castro@softtek.com>
2015-10-08 21:30:29 -0600

    Adding Libusb-1.0/Libusb0.1 wrapper class

---
+++ UsbWrapper/UsbWrapper/libusbwrapper.cpp
@@ -0,0 +1,7 @@
+#include "libusbwrapper.h"
+
+LibUsbWrapper::LibUsbWrapper()
+{
+
+}
+
---
+++ UsbWrapper/UsbWrapper/libusbwrapper.h
@@ -0,0 +1,12 @@
+#ifndef LIBUSBWRAPPER_H
+#define LIBUSBWRAPPER_H
+
+#include <QObject>
+
+class LibUsbWrapper : public UsbWrapper
+{
+public:
+    LibUsbWrapper();
+};
+
+#endif // LIBUSBWRAPPER_H
